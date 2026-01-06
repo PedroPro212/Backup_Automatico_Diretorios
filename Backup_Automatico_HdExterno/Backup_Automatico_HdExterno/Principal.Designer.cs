4df9ea756d8630cb7a9991727893835b7ae88611
Pedro Oliveira <pedrogamer212004@gmail.com>
2026-01-05 22:17:39 -0300

    feat: Software
    Sistema para selecionar diretórios

---
+++ Backup_Automatico_HdExterno/Backup_Automatico_HdExterno/Principal.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Backup_Automatico_HdExterno
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxPastas = new System.Windows.Forms.ListBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkExibirCMD = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(199, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AUTOMATIZAR BACKUP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(556, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Selecione pastas para copiar automaticamente para seu HD Externo";
+            // 
+            // listBoxPastas
+            // 
+            this.listBoxPastas.FormattingEnabled = true;
+            this.listBoxPastas.Location = new System.Drawing.Point(130, 261);
+            this.listBoxPastas.Name = "listBoxPastas";
+            this.listBoxPastas.Size = new System.Drawing.Size(435, 108);
+            this.listBoxPastas.TabIndex = 2;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.Location = new System.Drawing.Point(130, 143);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(134, 29);
+            this.btnAdicionar.TabIndex = 3;
+            this.btnAdicionar.Text = "Selecionar pastas";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover.Location = new System.Drawing.Point(431, 143);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(134, 29);
+            this.btnRemover.TabIndex = 4;
+            this.btnRemover.Text = "Remover pastas";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDestino.Location = new System.Drawing.Point(130, 210);
+            this.txtDestino.Multiline = true;
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(435, 38);
+            this.txtDestino.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(431, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 29);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Gerar .bat";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnGerarBat_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(127, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Destino do backup:";
+            // 
+            // chkExibirCMD
+            // 
+            this.chkExibirCMD.AutoSize = true;
+            this.chkExibirCMD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkExibirCMD.Location = new System.Drawing.Point(130, 389);
+            this.chkExibirCMD.Name = "chkExibirCMD";
+            this.chkExibirCMD.Size = new System.Drawing.Size(272, 21);
+            this.chkExibirCMD.TabIndex = 8;
+            this.chkExibirCMD.Text = "Exibir janela do CMD durante o backup";
+            this.chkExibirCMD.UseVisualStyleBackColor = true;
+            this.chkExibirCMD.CheckedChanged += new System.EventHandler(this.chkExibirCMD_CheckedChanged);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 462);
+            this.Controls.Add(this.chkExibirCMD);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtDestino);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.listBoxPastas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Principal";
+            this.ShowIcon = false;
+            this.Text = "Automatizador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxPastas;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkExibirCMD;
+    }
+}
+
---
+++ Backup_Automatico_HdExterno/Backup_Automatico_HdExterno/Principal.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace Backup_Automatico_HdExterno
+{
+    public partial class Principal : Form
+    {
+        private BackupConfig config;
+        private readonly string configPath = "BackupConfig.json";
+
+        public Principal()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            config = LoadConfig();
+
+            txtDestino.Text = config.Destino;
+            chkExibirCMD.Checked = config.ExibirCMD;
+            AtualizarLista();
+        }
+
+
+        public class BackupConfig
+        {
+            public string Destino { get; set; } = "";
+            public List<string> Pastas { get; set; } = new List<string>();
+
+            public bool ExibirCMD { get; set; } = true; // padrão: visível
+        }
+
+        private BackupConfig LoadConfig()
+        {
+            if (!File.Exists(configPath))
+                return new BackupConfig();
+
+            string json = File.ReadAllText(configPath);
+            return JsonSerializer.Deserialize<BackupConfig>(json) ?? new BackupConfig();
+        }
+
+        private void SaveConfig()
+        {
+            string json = JsonSerializer.Serialize(
+                config,
+                new JsonSerializerOptions { WriteIndented = true }
+            );
+
+            File.WriteAllText(configPath, json);
+        }
+
+        private void AtualizarLista()
+        {
+            listBoxPastas.Items.Clear();
+
+            foreach (string pasta in config.Pastas)
+                listBoxPastas.Items.Add(pasta);
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (!config.Pastas.Contains(dialog.SelectedPath))
+                    {
+                        config.Pastas.Add(dialog.SelectedPath);
+                        SaveConfig();
+                        AtualizarLista();
+                    }
+                }
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (listBoxPastas.SelectedItem == null)
+                return;
+
+            string pasta = listBoxPastas.SelectedItem.ToString();
+
+            config.Pastas.Remove(pasta);
+            SaveConfig();
+            AtualizarLista();
+        }
+
+        private void chkExibirCMD_CheckedChanged(object sender, EventArgs e)
+        {
+            config.ExibirCMD = chkExibirCMD.Checked;
+            SaveConfig();
+        }
+
+        void GerarBat(string destino, List<string> pastas, bool exibirCMD)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("@echo off");
+            sb.AppendLine(":: Verifica se está rodando como administrador");
+            sb.AppendLine("net session >nul 2>&1");
+            sb.AppendLine("if %errorLevel% neq 0 (");
+            sb.AppendLine("    powershell -Command \"Start-Process '%~f0' -Verb RunAs\"");
+            sb.AppendLine("    exit");
+            sb.AppendLine(")");
+            sb.AppendLine();
+
+            // Só minimiza se o usuário NÃO quiser ver o CMD
+            if (!exibirCMD)
+            {
+                sb.AppendLine("if \"%~1\" neq \"min\" (");
+                sb.AppendLine("    start \"\" /min \"%~f0\" min");
+                sb.AppendLine("    exit");
+                sb.AppendLine(")");
+                sb.AppendLine();
+            }
+
+            sb.AppendLine("set DATA=%date:~-4%%date:~3,2%%date:~0,2%");
+            sb.AppendLine();
+            sb.AppendLine($"set DESTINO={destino}");
+            sb.AppendLine("set LOG=C:\\BackupLogs\\backup_%DATA%.log");
+            sb.AppendLine();
+            sb.AppendLine("if not exist C:\\BackupLogs mkdir C:\\BackupLogs");
+            sb.AppendLine();
+
+            sb.AppendLine(":AGUARDA_HD");
+            sb.AppendLine("if not exist E:\\ (");
+            sb.AppendLine("    timeout /t 5 >nul");
+            sb.AppendLine("    goto AGUARDA_HD");
+            sb.AppendLine(")");
+            sb.AppendLine();
+
+            foreach (var origem in pastas)
+            {
+                string nome = new DirectoryInfo(origem).Name;
+
+                // Progresso só aparece com CMD visível
+                string flags = exibirCMD
+                    ? "/E /R:2 /W:5 /TEE /LOG+:%LOG%"
+                    : "/E /R:2 /W:5 /NFL /NDL /NJH /NJS /LOG+:%LOG%";
+
+                sb.AppendLine(
+                    $"robocopy \"{origem}\" \"%DESTINO%\\{nome}\" {flags}"
+                );
+            }
+
+            sb.AppendLine();
+            sb.AppendLine("exit");
+
+            File.WriteAllText("backup_automatico.bat", sb.ToString(), Encoding.ASCII);
+        }
+
+        private void btnGerarBat_Click(object sender, EventArgs e)
+        {
+            if (config.Pastas.Count == 0)
+            {
+                MessageBox.Show("Nenhuma pasta selecionada.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtDestino.Text))
+            {
+                MessageBox.Show("Informe o diretório de destino do backup.");
+                return;
+            }
+
+            config.Destino = txtDestino.Text.Trim();
+            SaveConfig();
+
+            GerarBat(config.Destino, config.Pastas, config.ExibirCMD);
+
+            MessageBox.Show("Arquivo backup_automatico.bat gerado com sucesso!");
+        }
+
+    }
+}
